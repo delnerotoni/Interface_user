26080631a07f6a55f9dccdd8adf271a968febe2d
delnerotoni <delnerotoni@gmail.com>
2021-10-21 13:06:05 -0300

    Iniciando o Front com Material-UI

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Inicio from './pages/client/painel';
+
+function App() {
+return (
+    <div className="App">
+        {/*<header className="App-header">
+            <h1>INTERFACE DE USUÁRIO PANORAMA_ID</h1>
+        </header>*/}
+        <Inicio />
+     </div>
+    );
+}
+
+export default App;
